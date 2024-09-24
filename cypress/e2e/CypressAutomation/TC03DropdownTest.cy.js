87d780368b1a2e87a96ed00fd30e1c9de16f792b
harafit <harafittesting@gmail.com>
2024-09-24 16:02:40 +0530

    Updated Dropdown Code

--- cypress/e2e/CypressAutomation/TC03DropdownTest.cy.js
+++ cypress/e2e/CypressAutomation/TC03DropdownTest.cy.js
@@ -1,6 +1,6 @@
 describe('Dropdown Demo', ()=>{
 
-    it('Dropdown Demo', ()=>{
+    it('Dropdown Demo with select tag', ()=>{
 
         cy.visit('https://www.zoho.com/commerce/free-demo.html');
 
@@ -21,4 +21,52 @@ describe('Dropdown Demo', ()=>{
         cy.get('[name="Email"]').type('Demo Application');
         
     })
+
+    it('Dropdown without select tag', function(){
+
+        cy.visit('https://www.dummyticket.com/dummy-ticket-for-visa-application/')
+        
+        cy.get('#select2-billing_country-container').click();
+
+        cy.get('.select2-search__field').type('Argentina').type('{enter}');
+
+        cy.get('#select2-billing_country-container').should('have.text','Argentina');
+
+        cy.get('#select2-billing_state-container').click();
+
+        cy.get('.select2-search__field').type('Chubut{enter}');
+
+        cy.get('#select2-billing_state-container').should('have.text', 'Chubut');
+
+    })
+    it.only('Dynamic Dropdown', function(){
+
+
+        cy.visit('https://www.google.com')
+        cy.get('#APjFqb').type('cypress automation');
+
+        cy.wait(2000);
+        //cy.get('div.wM6W7d').should('have.length', '13');
+
+        //cy.contains('cypress automation jobs').click();
+        //cy.contains('Language').click();
+
+        //cy.get('.classname').contains('address').click();
+
+        cy.get('div.wM6W7d').each(($el, index, $list)=>{
+
+            if($el.text() == 'cypress automation tool')
+            {
+                cy.wrap($el).click();
+            }
+            
+        })
+
+    })
+
+
 })
+
+
+
+//for loop ---> for(int i=0; i<10; i++)
